fad20307469fc0f72aeabcca5291efb9f49d8b61
periem <perier.emilie@hotmail.fr>
2017-12-20 12:27:09 +0100

    third commit

--- app/src/main/java/com/example/perie/androboum/MapsActivity.java
+++ app/src/main/java/com/example/perie/androboum/MapsActivity.java
@@ -2,17 +2,33 @@ package com.example.perie.androboum;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private List<Profil> userList;
+    private Profil me;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +38,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // on demande une instance du mécanisme d'authentification
+        final FirebaseAuth auth = FirebaseAuth.getInstance();
+        // la méthode ci-dessous renvoi l'utilisateur connecté ou null si personne
+        if (auth.getCurrentUser() != null) {
+            String me_email = auth.getCurrentUser().getEmail().toString();
+            System.out.println(me_email);
+
+        } else {
+            // do nothing
+        }
+
+        //récupération de la liste d'utilisateurs
+        userList = new ArrayList<>();
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                userList.clear();
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    userList.add(child.getValue(Profil.class));
+                }
+               
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.v("AndroBoum", "loadPost:onCancelled", databaseError.toException());
+            }
+        };
+        mDatabase.addValueEventListener(postListener);
+
+        if (mMap != null){
+            onMapReady(mMap);
+        }
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (me != null) {
+            //LatLng maPosition = new LatLng(me.getLatitude(), me.getLongitude());
+           // mMap.addMarker(new MarkerOptions().position(maPosition).title(me.getEmail()));
+            //mMap.moveCamera(CameraUpdateFactory.newLatLng(maPosition));
+        }
     }
+
 }
